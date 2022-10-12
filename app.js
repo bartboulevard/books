d93c0c3ade9eb575c8f7a86abefeb36aaa92cbc5
Kaspar Pedaja <kaspar.pedaja@voco.ee>
2022-10-12 11:14:07 +0300

    As an user, I am able to add and delete the book of my choice to a list [delivers #183521128]

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
-const form = document.querySelector('form')
-const bookInput = document.querySelector('#book')
-const booklist = document.querySelector('ul')
+const form = document.querySelector('#add-book')
+const addTitle = document.querySelector('#title')
+const addAuthor = document.querySelector('#author')
+const addISBN = document.querySelector('#isbn')
+const table = document.querySelector('table')
+const taskDelete = document.querySelector('table')
 
-form.addEventListener('submit', addBook)
-booklist.addEventListener('click', deleteBook)
+form.addEventListener('click', addBook)
+taskDelete.addEventListener('click', deleteBook)
 
+function addBook(e){
+    let row = table.insertRow()
+    let td = document.createElement('td')
+    td.appendChild(document.createTextNode(addTitle.value))
+    td.appendChild(document.createTextNode(addAuthor.value))
+    td.appendChild(document.createTextNode(addISBN.value))
+    const a = document.createElement('a')
+    a.appendChild(document.createTextNode('X'))
+    a.className = 'blue-text text-darken 2 secondary-content'
+    a.setAttribute('href', '#')
+    td.appendChild(a)
+    row.appendChild(td)
+}
+
+function deleteBook(e){
+    if(e.target.textContent === 'X'){
+        if(confirm('Are you sure you want to delete this value?')){}
+        e.target.parentElement.parentNode.remove()
+        console.log(e.target.parentNode)
+    }
+}
